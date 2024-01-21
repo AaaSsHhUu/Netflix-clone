71cc2db393c64223276b127c7a93c3bf15defc4c
Ashu Negi <anegi6723@gmail.com>
2024-01-21 12:07:17 +0530

    fixing issues in faq section

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const faqItems = document.querySelectorAll(".faq-item");
+const faqs = document.querySelectorAll(".faq");
 
-for(item of faqItems){
-    item.addEventListener("click",()=>{
-        let ques = document.querySelector(".faq-ques");
-        let ans = document.querySelector(".faq-ans");
-
-        ques.children[1].classList.toggle("rotate");
-        ans.classList.toggle("active");
+faqs.forEach((faq)=>{
+    faq.addEventListener("click",()=>{
+        faq.classList.toggle("active");
     })
-}
+})
